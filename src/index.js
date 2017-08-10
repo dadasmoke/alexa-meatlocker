177a4d47b6a12566cc45f3f878915752d63e9012
Jason Webb <jason.webb@rockfishdigital.com>
2017-08-10 15:23:09 -0400

    Finalized CookingIntent v1
    CookingIntent now accepts requests and provides responses as expected

--- src/index.js
+++ src/index.js
@@ -23,40 +23,6 @@ var handlers = {
         this.attributes['repromptSpeech'] = this.t("WELCOME_REPROMPT");
         this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
     },
-    'RecipeIntent': function () {
-        var itemSlot = this.event.request.intent.slots.Item;
-        var itemName;
-        if (itemSlot && itemSlot.value) {
-            itemName = itemSlot.value.toLowerCase();
-        }
-
-        var cardTitle = this.t("DISPLAY_CARD_TITLE", this.t("SKILL_NAME"), itemName);
-        var recipes = this.t("RECIPES");
-        var recipe = recipes[itemName];
-        var textResponse;
-        recipe = recipe.finishingTemp;
-        textResponse = 'the finishing temperature for ' + itemName + ' is ' + recipe + ' degrees';
-
-        if (recipe) {
-            this.attributes['speechOutput'] = textResponse;
-            this.attributes['repromptSpeech'] = this.t("RECIPE_REPEAT_MESSAGE");
-            this.emit(':tellWithCard', textResponse, this.attributes['repromptSpeech'], cardTitle, textResponse);
-        } else {
-            var speechOutput = this.t("RECIPE_NOT_FOUND_MESSAGE");
-            var repromptSpeech = this.t("RECIPE_NOT_FOUND_REPROMPT");
-            if (itemName) {
-                speechOutput += this.t("RECIPE_NOT_FOUND_WITH_ITEM_NAME", itemName);
-            } else {
-                speechOutput += this.t("RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME");
-            }
-            speechOutput += repromptSpeech;
-
-            this.attributes['speechOutput'] = speechOutput;
-            this.attributes['repromptSpeech'] = repromptSpeech;
-
-            this.emit(':ask', speechOutput, repromptSpeech);
-        }
-    },
     'CookingIntent': function () {
         var itemSlot = this.event.request.intent.slots.Item;
         var itemName;
@@ -68,147 +34,10 @@ var handlers = {
         var recipes = this.t("RECIPES");
         var recipe = recipes[itemName];
         var textResponse;
-        recipe = recipe.cookingTemp;
-        textResponse = 'the cooking temperature for ' + itemName + ' is ' + recipe + ' degrees';
 
         if (recipe) {
-            this.attributes['speechOutput'] = textResponse;
-            this.attributes['repromptSpeech'] = this.t("RECIPE_REPEAT_MESSAGE");
-            this.emit(':tellWithCard', textResponse, this.attributes['repromptSpeech'], cardTitle, textResponse);
-        } else {
-            var speechOutput = this.t("RECIPE_NOT_FOUND_MESSAGE");
-            var repromptSpeech = this.t("RECIPE_NOT_FOUND_REPROMPT");
-            if (itemName) {
-                speechOutput += this.t("RECIPE_NOT_FOUND_WITH_ITEM_NAME", itemName);
-            } else {
-                speechOutput += this.t("RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME");
-            }
-            speechOutput += repromptSpeech;
-
-            this.attributes['speechOutput'] = speechOutput;
-            this.attributes['repromptSpeech'] = repromptSpeech;
-
-            this.emit(':ask', speechOutput, repromptSpeech);
-        }
-    },
-    'CookTimeIntent': function () {
-        var itemSlot = this.event.request.intent.slots.Item;
-        var unitSlot = this.event.request.intent.slots.Unit;
-        var itemName;
-        if (itemSlot && itemSlot.value) {
-            itemName = itemSlot.value.toLowerCase();
-        }
-
-        var cardTitle = this.t("DISPLAY_CARD_TITLE", this.t("SKILL_NAME"), itemName);
-        var recipes = this.t("RECIPES");
-        var recipe = recipes[itemName];
-        var textResponse;
-        var unit = recipe.unit;
-        recipe = recipe.smokingTime;
-        if (unitSlot && unitSlot.value){
-            var unitName = unitSlot.value;
-            textResponse = recipe.unit[ unitName ];
-        } else {
-            
-            this.attributes['repromptSpeech'] = this.t("RECIPE_REPEAT_MESSAGE");
-            var repromptSpeech = this.t("WEIGHT_NOT_FOUND_REPROMPT");
-            speechOutput = repromptSpeech;
-
-            this.attributes['speechOutput'] = speechOutput;
-            this.attributes['repromptSpeech'] = repromptSpeech;
-
-            this.emit(':ask', speechOutput, repromptSpeech);
-
-        }
-        if (unit==='lb') {
-
-        }
-        textResponse = 'the cooking temperature for ' + itemName + ' is ' + recipe + ' degrees';
-
-        if (recipe) {
-            this.attributes['speechOutput'] = textResponse;
-            this.attributes['repromptSpeech'] = this.t("RECIPE_REPEAT_MESSAGE");
-            this.emit(':tellWithCard', textResponse, this.attributes['repromptSpeech'], cardTitle, textResponse);
-        } else {
-            var speechOutput = this.t("RECIPE_NOT_FOUND_MESSAGE");
-            var repromptSpeech = this.t("RECIPE_NOT_FOUND_REPROMPT");
-            if (itemName) {
-                speechOutput += this.t("RECIPE_NOT_FOUND_WITH_ITEM_NAME", itemName);
-            } else {
-                speechOutput += this.t("RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME");
-            }
-            speechOutput += repromptSpeech;
-
-            this.attributes['speechOutput'] = speechOutput;
-            this.attributes['repromptSpeech'] = repromptSpeech;
-
-            this.emit(':ask', speechOutput, repromptSpeech);
-        }
-    },
-    'VocabIntent': function () {
-        var itemSlot = this.event.request.intent.slots.Item;
-        var partSlot = this.event.request.intent.slots.Part;
-
-        var itemName;
-        if (itemSlot && itemSlot.value) {
-            itemName = itemSlot.value.toLowerCase();
-        }
-
-        var cardTitle = this.t("DISPLAY_CARD_TITLE", this.t("SKILL_NAME"), itemName);
-        var recipes = this.t("RECIPES");
-        var recipe = recipes[ itemName ];
-
-        var textResponse;
-        if (partSlot && partSlot.value){
-            var partName = partSlot.value;
-            textResponse = recipe.parts[ partName ];
-        } else {
-            textResponse = itemName + ' is a ' + recipe.definition; // + ' at ' + recipe.finishingTemp + ' degrees';
-        }
-
-        if (recipe) {
-            this.attributes['speechOutput'] = textResponse;
-            this.attributes['repromptSpeech'] = this.t("RECIPE_REPEAT_MESSAGE");
-            this.emit(':tellWithCard', textResponse, this.attributes['repromptSpeech'], cardTitle, textResponse);
-        } else {
-            var speechOutput = this.t("RECIPE_NOT_FOUND_MESSAGE");
-            var repromptSpeech = this.t("RECIPE_NOT_FOUND_REPROMPT");
-            if (itemName) {
-                speechOutput += this.t("RECIPE_NOT_FOUND_WITH_ITEM_NAME", itemName);
-            } else {
-                speechOutput += this.t("RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME");
-            }
-            speechOutput += repromptSpeech;
-
-            this.attributes['speechOutput'] = speechOutput;
-            this.attributes['repromptSpeech'] = repromptSpeech;
-
-            this.emit(':ask', speechOutput, repromptSpeech);
-        }
-    },
-    'PartIntent': function () {
-        var itemSlot = this.event.request.intent.slots.Item;
-        var partSlot = this.event.request.intent.slots.Part;
-
-        var itemName;
-        if (itemSlot && itemSlot.value) {
-            itemName = itemSlot.value.toLowerCase();
-        }
-
-        var cardTitle = this.t("DISPLAY_CARD_TITLE", this.t("SKILL_NAME"), itemName);
-        var recipes = this.t("RECIPES");
-        var recipe = recipes[ itemName ];
-
-        var textResponse;
-        recipe = recipe.parts;
-        var partName = partSlot.value;
-        textResponse = 'The parts for ' + itemName + ' are ' + recipe;
-        
-
-        if (recipe) {
-            this.attributes['speechOutput'] = textResponse;
-            this.attributes['repromptSpeech'] = this.t("RECIPE_REPEAT_MESSAGE");
-            this.emit(':tellWithCard', textResponse, this.attributes['repromptSpeech'], cardTitle, textResponse);
+            textResponse = 'The cooking temperature for ' + itemName + ' is ' + recipe.cookingTemp + ' degrees Fahrenheit';
+            this.emit(':tell', textResponse, null, cardTitle, textResponse);
         } else {
             var speechOutput = this.t("RECIPE_NOT_FOUND_MESSAGE");
             var repromptSpeech = this.t("RECIPE_NOT_FOUND_REPROMPT");
@@ -228,9 +57,6 @@ var handlers = {
     'AMAZON.HelpIntent': function () {
         this.attributes['speechOutput'] = this.t("HELP_MESSAGE");
         this.attributes['repromptSpeech'] = this.t("HELP_REPROMPT");
-        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
-    },
-    'AMAZON.RepeatIntent': function () {
         this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
     },
     'AMAZON.StopIntent': function () {
@@ -253,10 +79,11 @@ var languageStrings = {
     "en-US": {
         "translation": {
             "RECIPES": recipes.RECIPE_EN_US,
-            "SKILL_NAME": "Minecraft Helper",
+            "SKILL_NAME": "Meat Locker",
             "WELCOME_MESSAGE": "Welcome to %s. You can ask a question like, what\'s the recipe for a chest? ... Now, what can I help you with.",
             "WELCOME_REPROMPT": "For instructions on what you can say, please say help me.",
             "DISPLAY_CARD_TITLE": "%s  - Recipe for %s.",
+            "DISPLAY_CARD_TITLE_COOKINGTEMP": "%s  - Temperature for %s.",
             "HELP_MESSAGE": "You can ask questions such as, what\'s the recipe, or, you can say exit...Now, what can I help you with?",
             "HELP_REPROMPT": "You can say things like, what\'s the recipe, or you can say exit...Now, what can I help you with?",
             "STOP_MESSAGE": "Goodbye!",
@@ -267,34 +94,5 @@ var languageStrings = {
             "WEIGHT_NOT_FOUND_REPROMPT": "How much does it weigh?",
             "RECIPE_NOT_FOUND_REPROMPT": "What else can I help with?"
         }
-    }/*,
-    "en-US": {
-        "translation": {
-            "RECIPES" : recipes.RECIPE_EN_US,
-            "SKILL_NAME" : "American Minecraft Helper"
-        }
-    },
-    "en-GB": {
-        "translation": {
-            "RECIPES": recipes.RECIPE_EN_GB,
-            "SKILL_NAME": "British Minecraft Helper"
-        }
-    },
-    "de": {
-        "translation": {
-            "RECIPES" : recipes.RECIPE_DE_DE,
-            "SKILL_NAME" : "Assistent für Minecraft in Deutsch",
-            "WELCOME_MESSAGE": "Willkommen bei %s. Du kannst beispielsweise die Frage stellen: Welche Rezepte gibt es für eine Truhe? ... Nun, womit kann ich dir helfen?",
-            "WELCOME_REPROMPT": "Wenn du wissen möchtest, was du sagen kannst, sag einfach „Hilf mir“.",
-            "DISPLAY_CARD_TITLE": "%s - Rezept für %s.",
-            "HELP_MESSAGE": "Du kannst beispielsweise Fragen stellen wie „Wie geht das Rezept für“ oder du kannst „Beenden“ sagen ... Wie kann ich dir helfen?",
-            "HELP_REPROMPT": "Du kannst beispielsweise Sachen sagen wie „Wie geht das Rezept für“ oder du kannst „Beenden“ sagen ... Wie kann ich dir helfen?",
-            "STOP_MESSAGE": "Auf Wiedersehen!",
-            "RECIPE_REPEAT_MESSAGE": "Sage einfach „Wiederholen“.",
-            "RECIPE_NOT_FOUND_MESSAGE": "Tut mir leid, ich kenne derzeit ",
-            "RECIPE_NOT_FOUND_WITH_ITEM_NAME": "das Rezept für %s nicht. ",
-            "RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME": "dieses Rezept nicht. ",
-            "RECIPE_NOT_FOUND_REPROMPT": "Womit kann ich dir sonst helfen?"
-        }
-    }*/
+    }
 };
